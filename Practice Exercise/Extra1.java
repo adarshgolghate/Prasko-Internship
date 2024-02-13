e5f6ed3aaa5da5aa2b2eba3e771ef101ddcba9e1
Adarsh Golghate <adarshgolghate2002@gmail.com>
2024-02-13 09:05:42 +0530

    .

--- Practice Exercise/Extra1.java
+++ Practice Exercise/Extra1.java
@@ -18,28 +18,28 @@ public class Extra1 {
         System.out.println(" ");
         //this for loop will execute 5 times
         for(byte i=0; i<=4;i++){
-                //checking the condition if the value is greater than 1 then
-                if(date>1){
-                    for(byte k=1; k<date; k++){
-                        System.out.print("  \t"); 
+             //checking the condition if the value is greater than 1 then
+             if(date>1){
+                 for(byte k=1; k<date; k++){
+                    System.out.print("  \t"); 
+            }
+             date=0;
+            }
+            else{
+                //this loop will print the dates till 30
+                for(byte j=date; j<=day.length;j++ ){
+                   
+                    System.out.print(x +"\t");
+                    x++;
+                    if(x==31){
+                        break;
                     }
-                    date=0;
-                }
-                else{
-                    //this loop will print the dates till 30
-                    for(byte j=date; j<=day.length;j++ ){
-                       
-                        System.out.print(x +"\t");
-                        x++;
-                        if(x==31){
-                            break;
-                        }
-                        else{                
-                            continue;                
-                        }
-                    } 
-                    System.out.println(" ");                      
-                }         
+                    else{                
+                        continue;                
+                    }
+                } 
+                System.out.println(" ");                      
+            }         
         }
         
     }
