3d9f98632f0532243375a867f09dd4a7d70b3267
Adarsh Golghate <adarshgolghate2002@gmail.com>
2024-02-13 15:04:43 +0530

    gjh

--- Practice Exercise/Extra2.java
+++ Practice Exercise/Extra2.java
@@ -5,20 +5,25 @@ class AllProgram{
     
     void checkBalance(double a){
         System.out.println("Wel-Come User");
+        System.out.println(" ");
         System.out.print("Your balance is "+a);
     }
     void withdrawMoney(double b){
         Scanner scanMoney = new Scanner(System.in);
         System.out.print("Enter your amount");
         double withdraw = scanMoney.nextInt();
-        b = b-withdraw;
+        System.out.println(" ");
+         b = b-withdraw;
+        System.out.println(" ");
         System.out.println("Remaining balance is "+b);
     }
     void depositMoney(double c){
         Scanner scanMoney = new Scanner(System.in);
         System.out.println("Enter your amount");
         double deposit = scanMoney.nextInt();
+        System.out.println(" ");
         c = c + deposit;
+        System.out.println(" ");
         System.out.println("Your balance is "+c);
     }
 }
@@ -27,32 +32,39 @@ public class Extra2 {
     //Switch-case can be used to handle the menu selection, and if-else can be used for additional validations.
     public static void main(String args[]){
         Scanner scan = new Scanner(System.in);
-        int test=0;
+        int test=1;
         //below section allows user to register himself
         System.out.println("--------Register Your Self--------");
-        System.out.println("Enter desired card number:");        
+        System.out.println(" ");
+        System.out.print("Enter desired card number:");        
         int cardNumber = scan.nextInt();
+        System.out.println(" ");
         System.out.print("Enter your pin:");
         int pinNumber = scan.nextInt();
+        System.out.println(" ");
         System.out.print("Conform your pin:");
         int conformPin = scan.nextInt();
+        System.out.println(" ");
         if(pinNumber == conformPin){
             System.out.println("Pin Generated Succefully!!!");
+            System.out.println(" ");
             System.out.println("Enter your deposit amount...");
+            System.out.println(" ");
         }
         else{
             System.out.print("Invalid Pin");
             pinNumber =0;
             conformPin = 0;
-        }
-        
+        }        
         double balance = scan.nextInt();
         //this while loop checks for verifying the user and making him to perform certain actions
         while(test<3){
             System.out.print("Enter your Card Number:");
             int name = scan.nextInt();
+            System.out.println(" ");
             System.out.print("Enter your pin");
             int pin = scan.nextInt();
+            System.out.println(" ");
             if(name == cardNumber && pin== pinNumber){
                 System.out.println("---------Menu---------");
                 System.out.println("Press\t1 for Checking Balance");
@@ -72,6 +84,7 @@ public class Extra2 {
             else{
                 System.out.println("Invalid Cridentials");
                 test++;
+                System.out.println(" ");
                 System.out.println("Number of attempts left "+(test-1));
             }
             
