33830ba693f1525034f7a678e6e9d1403cbd730c
Adarsh Golghate <adarshgolghate2002@gmail.com>
2024-02-13 14:43:50 +0530

    final of ATM Simulator

--- Practice Exercise/Extra2.java
+++ Practice Exercise/Extra2.java
@@ -1,27 +1,25 @@
+//importing all packages from utility package
 import java.util.*;
+//this class contains the 
 class AllProgram{
-    double balance=15000;
-    void checkbalance(){
+    
+    void checkBalance(double a){
         System.out.println("Wel-Come User");
-        System.out.print("Your balance is "+balance);
+        System.out.print("Your balance is "+a);
     }
-    void withdrawMone(){
-        Scanner sc = new Scanner(System.in);
+    void withdrawMoney(double b){
+        Scanner scanMoney = new Scanner(System.in);
         System.out.print("Enter your amount");
-        double withdraw = sc.nextInt();
-        balance = balance-withdraw;
-        System.out.print("Remaining balance is "+balance);
+        double withdraw = scanMoney.nextInt();
+        b = b-withdraw;
+        System.out.println("Remaining balance is "+b);
     }
-    void depositMoney(){
-        Scanner sc1 = new Scanner(System.in);
+    void depositMoney(double c){
+        Scanner scanMoney = new Scanner(System.in);
         System.out.println("Enter your amount");
-        double deposit = sc1.nextInt();
-        balance = balance + deposit;
-        System.out.print("Your balance is "+balance);
-    }
-    void changePin(){
-        
-        
+        double deposit = scanMoney.nextInt();
+        c = c + deposit;
+        System.out.println("Your balance is "+c);
     }
 }
 public class Extra2 {
@@ -30,26 +28,46 @@ public class Extra2 {
     public static void main(String args[]){
         Scanner scan = new Scanner(System.in);
         int test=0;
+        //below section allows user to register himself
+        System.out.println("--------Register Your Self--------");
+        System.out.println("Enter desired card number:");        
+        int cardNumber = scan.nextInt();
+        System.out.print("Enter your pin:");
+        int pinNumber = scan.nextInt();
+        System.out.print("Conform your pin:");
+        int conformPin = scan.nextInt();
+        if(pinNumber == conformPin){
+            System.out.println("Pin Generated Succefully!!!");
+            System.out.println("Enter your deposit amount...");
+        }
+        else{
+            System.out.print("Invalid Pin");
+            pinNumber =0;
+            conformPin = 0;
+        }
+        
+        double balance = scan.nextInt();
+        //this while loop checks for verifying the user and making him to perform certain actions
         while(test<3){
             System.out.print("Enter your Card Number:");
             int name = scan.nextInt();
             System.out.print("Enter your pin");
             int pin = scan.nextInt();
-            if(name == 15082002 && pin== 1508){
+            if(name == cardNumber && pin== pinNumber){
                 System.out.println("---------Menu---------");
                 System.out.println("Press\t1 for Checking Balance");
                 System.out.println("Press\t2 for Money Withradwal");
                 System.out.println("Press\t3 for Deposit Money");
-                System.out.println("Press\t4 for Pin Changing");
                 byte choice = scan.nextByte();
+                //onject of AllProgram class
                 AllProgram ap = new AllProgram();
                 switch (choice) {
-                    case 1: ap.checkbalance();break;
-                    case 2: ap.withdrawMone();break;
-                    case 3: ap.depositMoney();break;
-                    case 4: ap.changePin();break;
+                    case 1: ap.checkBalance(balance);break;
+                    case 2: ap.withdrawMoney(balance);break;
+                    case 3: ap.depositMoney(balance);break;                    
                     default:System.out.print("Choose a valid option");break;
                 }
+                test=3;
             }
             else{
                 System.out.println("Invalid Cridentials");
@@ -57,8 +75,7 @@ public class Extra2 {
                 System.out.println("Number of attempts left "+(test-1));
             }
             
-        }     
-    
+        } 
 
     }
 }
