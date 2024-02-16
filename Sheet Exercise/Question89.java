1b340d102e32d3200274bd47e984a6d9ef19717f
Adarsh Golghate <adarshgolghate2002@gmail.com>
2024-02-16 13:12:58 +0530

    jjj

---
+++ Sheet Exercise/Question89.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Question89 {
+    //write a program to take category as input and print pets in that category
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("----Menu----");
+        System.out.println("Mammal");
+        System.out.println("Aquatic");
+        System.out.println("Avian");
+        System.out.println("Rodent");
+        System.out.println("Reptile");
+        System.out.println("Enter your category..");
+        String str = scan.next();
+
+        switch (str) {
+            case "Mammal": System.out.println("DOG"); System.out.println("CAT"); System.out.println("RABBIT"); System.out.println("DOG");  break;
+            case "Aquatic": System.out.println("Fish"); break;
+            case "Avian": System.out.print("Bird");break;
+            case "Rodent": System.out.print("Hamster");System.out.println("Guinea Pig");break;
+            case "Reptile":System.out.print("Turtle"); System.out.print("Snake");System.out.print("Lizard");break;
+            default:System.out.print("Invalid entery");break;
+        }
+
+    }
+}
